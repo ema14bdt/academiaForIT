003d0892493aeadc11dc087575e182b427999820
Emanuel Romero <e.romero.1989@gmail.com.ar>
2025-09-12 12:36:40 -0300

    feat(domain): Add role enum to User entity

--- domain/src/entities/User.ts
+++ domain/src/entities/User.ts
@@ -1,13 +1,16 @@
 /**
- * @file Defines the User entity.
+ * @file Defines the User entity and roles.
  */
 
-export type UserRole = 'admin' | 'client';
+export enum Role {
+  CLIENT = 'CLIENT',
+  PROFESSIONAL = 'PROFESSIONAL',
+}
 
 export interface User {
   id: string;
   name: string;
   email: string;
   passwordHash: string; // Store hash, not plaintext password
-  role: UserRole;
+  role: Role;
 }
